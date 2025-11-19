135f055955fa5e299a682d98ffb5fb0be3c2a6c6
Londeka-Zikalala <Londekazikalala@gmail.com>
2025-11-19 23:05:43 +0200

    endoint ui

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import db from './db/db.js';
 import FavoriteMoviesDB from './service/FavoriteMovies.js';
-import movieRoutes from './routes/MoviesRoutes.js';
+import movieRoutes from './routes/moviesRoutes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
